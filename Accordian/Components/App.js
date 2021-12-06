5f46f4055971849704439782062375e228d34e20
Kunal Singh <46413519+ks1912@users.noreply.github.com>
2021-12-06 10:35:03 +0530

    First Commit

---
+++ Accordian/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import Accordian from './Accordian';
+import Search from './Search';
+
+// const items = [
+//     {title:"Welcome", content: "Hi Welcome to react"},
+//     {title:"Feuji", content: "Feuji is a growing up country located in India"},
+//     {title:"Location", content:"Feuji India office is in Hyderabad."}
+// ]
+// const App = () => {
+//     return ( <div>
+//                 <br/>
+//                 <Accordian items={items}/>
+//             </div> );
+// }
+ 
+export default App;
