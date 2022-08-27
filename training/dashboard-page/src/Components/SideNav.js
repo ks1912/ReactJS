cbe5fe6ef2070882abf26ee0f672f5e536b824ae
ks1912 <kunalsingh886312@gmail.com>
2022-08-27 21:54:01 +0530

    added dashboard project

---
+++ training/dashboard-page/src/Components/SideNav.js
@@ -0,0 +1,14 @@
+// Import React
+import React from "react";
+
+// Define Functional Component
+const SideNav = () => {
+  return (
+    <div className="col-sm-10 m-0 p-0">
+      <div style={{ backgroundColor: "#F1F1F1" }} className="p-2 text-center"></div>
+      <div style={{ backgroundColor: "#F1F1F1" }} className="p-2 text-center">classMa,e</div>
+    </div>
+  );
+};
+// Export the class
+export default SideNav;
